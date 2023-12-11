19dfda582bdea464914ca02453697bbf6a324eb4
Javier <javie@NoteParranda-2>
2023-12-10 21:47:53 -0300

    Detalle descripcion en mantencion

--- src/taller/Mantencion.java
+++ src/taller/Mantencion.java
@@ -14,16 +14,28 @@ public class Mantencion {
     private Date fec_sal;
     private boolean estado;
     private Vehiculo vehiculo;
+    private String descripcion;
+    private int kilometraje_man;
 
     public Mantencion() {
     }
 
-    public Mantencion(int id, Date fec_ing, Date fec_sal, boolean estado, Vehiculo vehiculo) {
+    public Mantencion(int id, Date fec_ing, Date fec_sal, boolean estado, Vehiculo vehiculo, String descripcion, int kilometraje_man) {
         this.id = id;
         this.fec_ing = fec_ing;
         this.fec_sal = fec_sal;
         this.estado = estado;
         this.vehiculo = vehiculo;
+        this.descripcion = descripcion;
+        this.kilometraje_man = kilometraje_man;
+    }
+
+    public int getKilometraje_man() {
+        return kilometraje_man;
+    }
+
+    public void setKilometraje_man(int kilometraje_man) {
+        this.kilometraje_man = kilometraje_man;
     }
 
     public int getId() {
@@ -65,6 +77,21 @@ public class Mantencion {
     public void setVehiculo(Vehiculo vehiculo) {
         this.vehiculo = vehiculo;
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     
     
+    public boolean verificaEstado(int estado) {
+        if (estado == 1){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
